91678fb5b0428aab91329ef9699f5860ac75c374
Masahiko Ebisuda <ebibibi@gmail.com>
2025-06-12 15:26:10 +0900

    Add setattribute.ps1 script to manage custom security attributes for excluded applications

---
+++ EntraID/全てのリソースに対しての対象外の意図せぬ挙動対策/setattribute.ps1
@@ -0,0 +1,52 @@
+# Microsoft Graph SDK に接続（対話型）
+Connect-MgGraph -Scopes "Application.ReadWrite.All", "Directory.ReadWrite.All", "CustomSecAttributeAssignment.ReadWrite.All", "Application.Read.All", "CustomSecAttributeAssignment.Read.All", "Application.Read.All"
+
+# 除外対象アプリの ObjectId または DisplayName のリストをここに記述
+$excludedAppNames = @(
+    "ConditionalAccessTest",
+    "Sample App A",
+    "Sample App B"
+)
+
+# 属性セットと定義名（既に作成済みのものを使用）
+$attributeSetName = "isExcludeApps"
+$attributeDefinitionName = "isExcludeApp"
+
+# すべての ServicePrincipal を取得（ページング対応）
+$allApps = @()
+$nextLink = "https://graph.microsoft.com/v1.0/servicePrincipals"
+do {
+    $result = Invoke-MgGraphRequest -Method GET -Uri $nextLink
+    $allApps += $result.value
+    $nextLink = $result.'@odata.nextLink'
+} while ($nextLink)
+
+Write-Host "合計アプリ数: $($allApps.Count)"
+
+# アプリごとに属性をセット
+foreach ($app in $allApps) {
+    $isExcluded = $false
+
+    if ($excludedAppNames -contains $app.DisplayName) {
+        $isExcluded = $true
+    }
+
+    $attributeValue = if ($isExcluded) { "true" } else { "false" }
+
+    # 属性セットの設定
+    $params = @{
+        customSecurityAttributes = @{
+            $attributeSetName = @{
+                "@odata.type" = "#Microsoft.DirectoryServices.CustomSecurityAttributeValue"
+                $attributeDefinitionName = $attributeValue
+            }
+        }
+    }
+
+    try {
+        Update-MgServicePrincipal -ServicePrincipalId $app.Id -BodyParameter $params
+        Write-Host "[OK] $($app.DisplayName): $attributeValue"
+    } catch {
+        Write-Warning "[NG] $($app.DisplayName): $($_.Exception.Message)"
+    }
+}
