ef4f7fee13325ec9158d21760415a2e486972dc3
Masahiko Ebisuda <ebibibi@gmail.com>
2025-11-04 16:09:16 +0900

    Refactor Find-OldUsers.ps1 for improved readability by restructuring the Get-ADUser filter and adding a Where-Object clause.

--- ActiveDirectory/Find-OldObjects/Find-OldUsers.ps1
+++ ActiveDirectory/Find-OldObjects/Find-OldUsers.ps1
@@ -1,7 +1,13 @@
 $oneYearAgo = (Get-Date).AddYears(-1)
 
-Get-ADUser -Filter {Enabled -eq $true -and lastLogonTimestamp -lt $oneYearAgo} -Properties lastLogonTimestamp |
-    Select-Object Name,
-                  SamAccountName,
-                  @{Name='LastLogonDate';Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} |
-    Sort-Object LastLogonDate
+Get-ADUser -Filter {
+    Enabled -eq $true -and
+    lastLogonTimestamp -lt $oneYearAgo
+} -Properties lastLogonTimestamp |
+Where-Object {
+    $_.SamAccountName -notmatch '^(IUSR_|IWAM_|ASPNET|Guest|krbtgt|DefaultAccount|WDAGUtilityAccount)'
+} |
+Select-Object Name,
+              SamAccountName,
+              @{Name='LastLogonDate';Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} |
+Sort-Object LastLogonDate
